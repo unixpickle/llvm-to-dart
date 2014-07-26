704404ba9edd59a7ded4874ee6683f5b4c6b6774
Alex Nichol <unixpickle@gmail.com>
2014-07-26 17:12:12 -0400

    Initial Commit

---
+++ LlvmToDart/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LlvmToDart
+//
+//  Created by Alex Nichol on 7/26/14.
+//  Copyright (c) 2014 Alex Nichol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
