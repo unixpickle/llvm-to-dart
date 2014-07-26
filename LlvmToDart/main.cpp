089e6d401aed3fd8ee636dabf99d460ecbc4f3c6
Alex Nichol <unixpickle@gmail.com>
2014-07-26 17:36:03 -0400

    attempt to parse LLVM IR file

--- LlvmToDart/main.cpp
+++ LlvmToDart/main.cpp
@@ -6,13 +6,32 @@
 //  Copyright (c) 2014 Alex Nichol. All rights reserved.
 //
 
+#define __STDC_LIMIT_MACROS
+#define __STDC_CONSTANT_MACROS
+
 #include <iostream>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/raw_ostream.h>
 
-int main(int argc, const char * argv[])
-{
+using namespace std;
 
-  // insert code here...
-  std::cout << "Hello, World!\n";
-    return 0;
+int main(int argc, const char * argv[]) {
+  if (argc != 2) {
+    cerr << "Usage: llvmtodart <some file.s>" << endl;
+    return 1;
+  }
+  
+  llvm::LLVMContext & context = llvm::getGlobalContext();
+  llvm::SMDiagnostic err;
+  llvm::Module * module = llvm::ParseIRFile(argv[1], err, context);
+  
+  if (!module) {
+    err.print("llvmtodart", llvm::errs());
+    return 1;
+  }
+  
+  return 0;
 }
 
